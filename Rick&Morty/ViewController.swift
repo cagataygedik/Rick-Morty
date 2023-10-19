194ba96fc9c3f6b39c6e01d8ddadadf1fac1113c
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-10-19 13:43:29 +0300

    Initial Commit

---
+++ Rick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
