d02988f6f1ecb9fe90971fab764a7944a6ae3773
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-10-23 20:09:35 +0300

    Writed our API request

--- Rick&Morty/APIClient/RMRequest.swift
+++ Rick&Morty/APIClient/RMRequest.swift
@@ -9,4 +9,44 @@ import Foundation
 
 final class RMRequest {
     
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    private let endpoint: RMEndpoint
+    private let pathComponents: [String]
+    private let queryParameters: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach({ string += "/\($0)" })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let arguementString = queryParameters.compactMap ({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += arguementString
+        }
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    public let httpMethod = "GET"
+    
+    public init(endpoint: RMEndpoint, pathComponents: [String] = [], queryParameters: [URLQueryItem] = []) {
+        self.endpoint = endpoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
 }
