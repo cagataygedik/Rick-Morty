f4c02614ea3e29fb6fb7f55173efeb97f376f2c5
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-10-23 14:40:19 +0300

    Created new Models

---
+++ Rick&Morty/Models/RMCharacterGender.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterGender.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 23.10.2023.
+//
+
+import Foundation
---
+++ Rick&Morty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 23.10.2023.
+//
+
+import Foundation
