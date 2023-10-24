4bf657bd6d59b2943180f46ac1807f81afdc8f5f
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-10-24 11:43:22 +0300

    Writed our API service

--- Rick&Morty/APIClient/RMService.swift
+++ Rick&Morty/APIClient/RMService.swift
@@ -12,10 +12,40 @@ final class RMService {
     
     private init() {}
     
+    enum RMServiceError: Error {
+        case failedToCreateRequest
+        case failedToGetData
+    }
+    
     public func execute<T: Codable>(
         _ request: RMRequest,
         expecting type: T.Type,
         completion: @escaping (Result<T, Error>) -> Void) {
+            guard let urlRequest = self.request(from: request) else {
+                completion(.failure(RMServiceError.failedToCreateRequest))
+                return
+            }
+            let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(error ?? RMServiceError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let result = try JSONDecoder().decode(type.self, from: data)
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
+    }
+    
+    private func request(from rmRequest: RMRequest) -> URLRequest? {
+        guard let url = rmRequest.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = rmRequest.httpMethod
+        return request
     }
 }
 
