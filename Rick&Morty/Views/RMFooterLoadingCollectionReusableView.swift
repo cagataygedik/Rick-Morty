338ec1b18fafbf3b2e799825ee05967bb77bdffc
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-01 15:48:57 +0300

    Developed spinner for character list view footer.

---
+++ Rick&Morty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RMFooterLoadingCollectionReusableView.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 30.10.2023.
+//
+
+import UIKit
+
+class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
