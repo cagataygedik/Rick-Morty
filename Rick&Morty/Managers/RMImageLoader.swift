705b68cfcab360558efdca06acf8034b78a3d18a
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-02 20:48:59 +0300

    We implemented a caching mechanism

---
+++ Rick&Morty/Managers/RMImageLoader.swift
@@ -0,0 +1,39 @@
+//
+//  RMImageLoader.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 2.11.2023.
+//
+
+import Foundation
+
+final class RMImageLoader {
+    static let shared = RMImageLoader()
+    
+    private var imageDataCache = NSCache<NSString, NSData>()
+    
+    private init(){}
+    
+    public func downloadImage(_ url: URL, completion: @escaping (Result<Data, Error>) -> Void) {
+        let key = url.absoluteString as NSString
+        if let data = imageDataCache.object(forKey: key) {
+            completion(.success(data as Data))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { [weak self] data, _, error in
+            guard let data = data, error == nil else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                return
+            }
+            let value = data as NSData
+            self?.imageDataCache.setObject(value, forKey: key)
+            completion(.success(data))
+        }
+        task.resume()
+    }
+    
+}
+
+
