a45cf1f9b619196e49d10f058789e383aa1fd149
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-05 21:46:43 +0300

    We assigned to detail view to it's delegate

--- Rick&Morty/Controllers/Other/RMCharacterDetailViewController.swift
+++ Rick&Morty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     private let viewModel: RMCharacterDetailViewViewModel
     
-    private let detailView = RMCharacterDetailView()
+    private let detailView: RMCharacterDetailView
     
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
+        self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
 
@@ -23,6 +24,9 @@ final class RMCharacterDetailViewController: UIViewController {
         title = viewModel.title
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShareButton))
         setupView()
+        
+        detailView.collectionView?.delegate = self
+        detailView.collectionView?.dataSource = self
     }
     
     private func setupView() {
@@ -41,5 +45,39 @@ final class RMCharacterDetailViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("error")
+    }
+}
+
+extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return viewModel.sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 8
+        case 2:
+            return 20
+        default:
+            return 1
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemPink
+        
+        if indexPath.section == 0 {
+            cell.backgroundColor = .systemBlue
+        } else if indexPath.section == 1 {
+            cell.backgroundColor = .systemCyan
+        } else {
+            cell.backgroundColor = .systemBrown
+        }
+        
+        return cell
     }
 }
