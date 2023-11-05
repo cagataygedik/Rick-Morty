b9b0d5b99599436652e96b94e14e0a6aba085e1e
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-05 19:18:23 +0300

    we added new variable for requesting url.

--- Rick&Morty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ Rick&Morty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -14,6 +14,10 @@ final class RMCharacterDetailViewViewModel {
         self.character = character
     }
     
+    public var requestUrl: URL? {
+        return URL(string: character.url)
+    }
+    
     public var title: String {
         character.name.uppercased()
     }
