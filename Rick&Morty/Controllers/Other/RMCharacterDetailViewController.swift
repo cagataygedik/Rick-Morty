fadf0729760d84f7911080c179d30e7f713a249e
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-06 18:12:28 +0300

    We are creating the view models to build character detail screen.

--- Rick&Morty/Controllers/Other/RMCharacterDetailViewController.swift
+++ Rick&Morty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -17,7 +17,7 @@ final class RMCharacterDetailViewController: UIViewController {
         self.detailView = RMCharacterDetailView(frame: .zero, viewModel: viewModel)
         super.init(nibName: nil, bundle: nil)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -54,30 +54,43 @@ extension RMCharacterDetailViewController: UICollectionViewDelegate, UICollectio
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
+        let sectionType = viewModel.sections[section]
+        
+        switch sectionType {
+        case .photo:
             return 1
-        case 1:
-            return 8
-        case 2:
-            return 20
-        default:
-            return 1
+        case .information(let viewModels):
+            return viewModels.count
+        case .episodes(let viewModels):
+            return viewModels.count
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .systemPink
+        let sectionType = viewModel.sections[indexPath.section]
         
-        if indexPath.section == 0 {
+        switch sectionType {
+        case .photo(let viewModel):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterPhotoCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterPhotoCollectionViewCell else {
+                fatalError()
+            }
+            cell.configure(with: viewModel)
+            cell.backgroundColor = .systemPink
+            return cell
+        case .information(let viewModels):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterInfoCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterInfoCollectionViewCell else {
+                fatalError()
+            }
+            cell.configure(with: viewModels[indexPath.row])
+            cell.backgroundColor = .systemOrange
+            return cell
+        case .episodes(let viewModels):
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterEpisodeCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterEpisodeCollectionViewCell else {
+                fatalError()
+            }
+            cell.configure(with: viewModels[indexPath.row])
             cell.backgroundColor = .systemBlue
-        } else if indexPath.section == 1 {
-            cell.backgroundColor = .systemCyan
-        } else {
-            cell.backgroundColor = .systemBrown
+            return cell
         }
-        
-        return cell
     }
 }
--- Rick&Morty/ViewModels/CharacterDetails/RMCharacterDetailViewViewModel.swift
+++ Rick&Morty/ViewModels/CharacterDetails/RMCharacterDetailViewViewModel.swift
@@ -10,16 +10,25 @@ import UIKit
 final class RMCharacterDetailViewViewModel {
     private let character: RMCharacter
     
-    enum SectionType: CaseIterable {
-        case photo
-        case information
-        case episodes
+    enum SectionType {
+        case photo(viewModel: RMCharacterPhotoCollectionViewCellViewModel)
+        case information(viewModel: [RMCharacterInfoCollectionViewCellViewModel])
+        case episodes(viewModel: [RMCharacterEpisodeCollectionViewCellViewModel])
     }
     
-    public let sections = SectionType.allCases
+    public var sections: [SectionType] = []
     
     init(character: RMCharacter) {
         self.character = character
+        setupSections()
+    }
+    
+    private func setupSections() {
+        sections = [
+            .photo(viewModel: .init()),
+            .information(viewModel: [.init(), .init(), .init(), .init()]),
+            .episodes(viewModel: [.init(), .init(), .init(), .init()])
+        ]
     }
     
     public var requestUrl: URL? {
