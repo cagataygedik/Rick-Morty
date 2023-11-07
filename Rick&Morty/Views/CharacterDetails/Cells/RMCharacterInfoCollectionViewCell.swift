701bf3566898ac14cc978a78bd7cb020e29b8305
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-07 18:59:26 +0300

    Building out Character Detail Info section

--- Rick&Morty/Views/CharacterDetails/Cells/RMCharacterInfoCollectionViewCell.swift
+++ Rick&Morty/Views/CharacterDetails/Cells/RMCharacterInfoCollectionViewCell.swift
@@ -10,20 +10,88 @@ import UIKit
 class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterInfoCollectionViewCell"
     
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .light)
+        label.text = "Dummy"
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        label.text = "Dummy"
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.contentMode = .scaleAspectFit
+        icon.image = UIImage(systemName: "globe.americas")
+        return icon
+    }()
+    
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor = .tertiarySystemBackground
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        addSubviews()
+        addConstraints()
     }
     
-    private func setupConstraints() {
-        
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            
+            titleLabel.topAnchor.constraint(equalTo: titleContainerView.topAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: titleContainerView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: titleContainerView.rightAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainerView.bottomAnchor),
+            
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
+            iconImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            iconImageView.heightAnchor.constraint(equalToConstant: 30),
+            iconImageView.widthAnchor.constraint(equalToConstant: 30),
+            
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            valueLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            valueLabel.bottomAnchor.constraint(equalTo: titleContainerView.topAnchor)
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+        valueLabel.text = nil
+        titleLabel.text = nil
+        iconImageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
         
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(titleContainerView)
+        contentView.addSubview(valueLabel)
+        contentView.addSubview(iconImageView)
+        titleContainerView.addSubview(titleLabel)
     }
     
     required init?(coder: NSCoder) {
