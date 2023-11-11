19abfb926004657046793402256091c673fc0320
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-11 20:16:25 +0300

    Implemented our caching to the network service.

--- Rick&Morty/APIClient/RMService.swift
+++ Rick&Morty/APIClient/RMService.swift
@@ -10,6 +10,8 @@ import Foundation
 final class RMService {
     static let shared = RMService()
     
+    private let cacheManager = RMAPICacheManager()
+    
     private init() {}
     
     enum RMServiceError: Error {
@@ -21,11 +23,22 @@ final class RMService {
         _ request: RMRequest,
         expecting type: T.Type,
         completion: @escaping (Result<T, Error>) -> Void) {
+            
+            if let cachedData = cacheManager.cachedResponse(for: request.endpoint, url: request.url) {
+                do {
+                    let result = try JSONDecoder().decode(type.self, from: cachedData)
+                    completion(.success(result))
+                } catch {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
             guard let urlRequest = self.request(from: request) else {
                 completion(.failure(RMServiceError.failedToCreateRequest))
                 return
             }
-            let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+            let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
                 guard let data = data, error == nil else {
                     completion(.failure(error ?? RMServiceError.failedToGetData))
                     return
@@ -33,6 +46,7 @@ final class RMService {
                 
                 do {
                     let result = try JSONDecoder().decode(type.self, from: data)
+                    self?.cacheManager.setCache(for: request.endpoint, url: request.url, data: data)
                     completion(.success(result))
                 } catch {
                     completion(.failure(error))
