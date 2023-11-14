6aa43c3fc55a8f67f82e81b216d4445b766ccf71
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-14 22:44:08 +0300

    We create our functions that we will develop later.

--- Rick&Morty/Views/EpisodeDetails/RMEpisodeDetailView.swift
+++ Rick&Morty/Views/EpisodeDetails/RMEpisodeDetailView.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 final class RMEpisodeDetailView: UIView {
+    private var viewModel: RMEpisodeDetailViewViewModel?
+    
+    private var collectionView: UICollectionView?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemBackground
+        addContraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addContraints() {
+        
+    }
+    
+    public func configure(with viewModel: RMEpisodeDetailViewViewModel) {
+        self.viewModel = viewModel
     }
 }
