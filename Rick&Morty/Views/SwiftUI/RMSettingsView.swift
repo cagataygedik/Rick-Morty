44ab997b579f93780476113cd01b8989d5362848
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-21 14:14:53 +0300

    Finished Settings page

--- Rick&Morty/Views/SwiftUI/RMSettingsView.swift
+++ Rick&Morty/Views/SwiftUI/RMSettingsView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct RMSettingsView: View {
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color.red)
+                        .padding(8)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(6)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+                
+                Spacer()
+            }
+            .padding(.bottom, 3)
+            .onTapGesture {
+                viewModel.onTapHandler(viewModel.type)
+            }
+        }
     }
 }
 
 #Preview {
-    RMSettingsView()
+    RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+        return RMSettingsCellViewModel(type: $0) { option in
+            
+        }
+    })))
 }
