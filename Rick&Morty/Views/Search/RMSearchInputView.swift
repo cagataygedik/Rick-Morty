8ba92cfc50fb3e5c3de38b4b3b061144358f5909
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-29 14:48:40 +0300

    created option buttons under the search bar.

--- Rick&Morty/Views/Search/RMSearchInputView.swift
+++ Rick&Morty/Views/Search/RMSearchInputView.swift
@@ -29,7 +29,6 @@ class RMSearchInputView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemGreen
         addSubviews()
         addConstraints()
     }
@@ -43,9 +42,48 @@ class RMSearchInputView: UIView {
         ])
     }
     
+    private func createOptionStackView() -> UIStackView {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.spacing = 6
+        addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 4),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -4),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        return stackView
+    }
+    
+    private func createOptionButtonView(with options: RMSearchInputViewViewModel.DynamicOption, tag: Int) -> UIButton {
+        let button = UIButton()
+        button.setAttributedTitle(NSAttributedString(string: options.rawValue, attributes: [.font: UIFont.systemFont(ofSize: 18, weight: .medium), .foregroundColor: UIColor.label]), for: .normal)
+        button.backgroundColor = .secondarySystemFill
+        button.tag = tag
+        button.layer.cornerRadius = 6
+        button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+        return button
+    }
+    
+    @objc private func didTapButton(_ sender: UIButton) {
+        guard let options = viewModel?.options else {
+            return
+        }
+        let tag = sender.tag
+        let selected = options[tag]
+    }
+    
     private func createOptionSelectionViews(options: [RMSearchInputViewViewModel.DynamicOption]) {
-        for option in options {
-            
+        let stackView = createOptionStackView()
+        
+        for x in 0..<options.count {
+            let option = options[x]
+            let button = createOptionButtonView(with: option, tag: x)
+            stackView.addArrangedSubview(button)
         }
     }
     
