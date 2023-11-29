3b838e69e887004c8880b579acad2129c88ab4a1
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-29 12:07:13 +0300

    Developed RMSearchInputViewViewModel.

--- Rick&Morty/ViewModels/Search/RMSearchInputViewViewModel.swift
+++ Rick&Morty/ViewModels/Search/RMSearchInputViewViewModel.swift
@@ -6,3 +6,70 @@
 //
 
 import Foundation
+
+final class RMSearchInputViewViewModel {
+    private let type: RMSearchViewController.Config.`Type`
+    
+    enum DynamicOption: String {
+        case status = "Status"
+        case gender = "Gender"
+        case locationType = "Location Type"
+        
+        var queryArgument: String {
+            switch self {
+            case .status:
+                return "status"
+            case .gender:
+                return "gender"
+            case .locationType:
+                return "type"
+            }
+        }
+        
+        var choices: [String] {
+            switch self {
+            case .status:
+                return ["alive", "dead", "unknown"]
+            case .gender:
+                return ["male", "female", "genderless", "unknown"]
+            case .locationType:
+                return ["cluster", "planet", "microverse"]
+            }
+        }
+    }
+    
+    init(type: RMSearchViewController.Config.`Type`) {
+        self.type = type
+    }
+    
+    public var hasDynamicOptions: Bool {
+        switch self.type {
+        case .character, .location:
+            return true
+        case .episode:
+            return false
+        }
+    }
+    
+    public var options: [DynamicOption] {
+        switch self.type {
+        case .character:
+            return [.status, .gender]
+        case .episode:
+            return []
+        case .location:
+            return [.locationType]
+        }
+    }
+    
+    public var searchPlaceholderText: String {
+        switch self.type {
+        case .character:
+            return "Look For Characters"
+        case .episode:
+            return "Search For Episodes"
+        case .location:
+            return "Search locations"
+        }
+    }
+}
