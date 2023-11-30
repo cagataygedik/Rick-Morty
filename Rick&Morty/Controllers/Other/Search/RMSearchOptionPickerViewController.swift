1f08557f3d9da70f353a14e43dda0750986c8f1d
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-11-30 12:07:53 +0300

    Developed bottom sheet option picker

---
+++ Rick&Morty/Controllers/Other/Search/RMSearchOptionPickerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RMSearchOptionPickerViewController.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 30.11.2023.
+//
+
+import UIKit
+
+class RMSearchOptionPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
