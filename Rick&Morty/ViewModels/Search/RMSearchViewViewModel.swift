380d70234b7aa8db975030c6803ef5ade08b1cf1
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-02 22:32:30 +0300

    Started developing execute search

--- Rick&Morty/ViewModels/Search/RMSearchViewViewModel.swift
+++ Rick&Morty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -13,14 +13,40 @@ final class RMSearchViewViewModel {
     private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOption, String)) -> Void)?
     private var searchText = ""
-    
-    
+    private var searchResultHandler: (() -> Void)?
+        
     init(config: RMSearchViewController.Config) {
         self.config = config
     }
     
+    public func registerSearchResultHandler(_ block: @escaping () -> Void) {
+        self.searchResultHandler = block
+    }
+    
     public func executeSearch() {
+        searchText = "Rick" //test
+        var queryParams: [URLQueryItem] = [URLQueryItem(name: "name", value:searchText)]
         
+        //add options
+        queryParams.append(contentsOf: optionMap.enumerated().compactMap({ _, element in
+            let key: RMSearchInputViewViewModel.DynamicOption = element.key
+            let value: String = element.value
+            return URLQueryItem(name: key.queryArgument, value: value)
+        }))
+        
+        //create request
+        let request = RMRequest(endpoint: config.type.endpoint, queryParameters: queryParams)
+        print(request.url?.absoluteString) //test
+        
+        //execute request
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print("\(model.results.count)")
+            case .failure(_):
+                break
+            }
+        }
     }
     
     public func set(query text: String) {
