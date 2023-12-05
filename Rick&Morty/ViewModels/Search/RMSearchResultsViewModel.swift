34dda319412e9129a93dc143685ff1fb676c1096
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-05 11:15:05 +0300

    Created new viewModel

---
+++ Rick&Morty/ViewModels/Search/RMSearchResultsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSearchResultsViewModel.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 4.12.2023.
+//
+
+import Foundation
+
+final class RMSearchResultsViewModel {
+    
+}
