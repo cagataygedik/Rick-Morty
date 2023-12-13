878ecd033b5898b9155ad0d94b2c3cbe39af26a4
Cagatay Gedik <cagatay.gedik@icloud.com>
2023-12-13 17:29:16 +0300

    Created loading footer view for location pagination.

---
+++ Rick&Morty/Views/RMTableLoadingFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  RMTableLoadingFooterView.swift
+//  Rick&Morty
+//
+//  Created by Celil Çağatay Gedik on 13.12.2023.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(spinner)
+        addConstraints()
+        spinner.startAnimating()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
